bcd4c6dbb217d3c51d490e796a438bb14093f56a
armin <mashadow023@gmail.com>
2024-10-30 16:52:04 +0330

    Add files via upload

---
+++ site3.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
